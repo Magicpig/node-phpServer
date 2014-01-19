4cbf6cbcb3e972073281c2f1da01a2d3253643b2
magicpig <54379436@qq.com>
2014-01-19 18:08:57 +0800

    frist
    frist

--- t.js
+++
@@ -1,9 +0,0 @@
-process.stdin.resume();
-process.stdin.setEncoding('utf8');
-
-process.stdin.on('data', function(chunk) {
-  console.log(chunk);
-});
-
-process.stdin.write('sudo cd /root');
-// process.stdout.write('sudo cd /root');
